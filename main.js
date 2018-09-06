6b25b7de600702fd02c83f4abfa4e11321cad3ef
Tomáš Roun <rountoma@fel.cvut.cz>
2018-09-07 01:23:27 +0200

    add online search

--- main.js
+++ main.js
@@ -6,156 +6,84 @@
     return Object.keys(obj)
   }
 
-  function resetResults(results, container) {
+  function setResults(translations, container) {
     while (container.firstChild) {
       container.removeChild(container.firstChild)
     }
 
-    results.forEach(r => {
+    translations.forEach(translation => {
       const span = document.createElement("span")
-      span.textContent = r
-      span.style.cssText = `
-        border-top: 1px dashed #bfbfbf;
-        box-sizing: border-box;
-        width: 150px;
-        padding: 8px 8px;
-      `
+      span.textContent = translation
+      span.classList.add("translation")
       container.appendChild(span)
     })
   }
 
-  const css = {
-    parent: [
-      "all: initial",
-      "box-sizing: border-box",
-      "position: absolute",
-      "display: none",
-      "width: 200px",
-      "height: auto",
-      "font-size: 14px",
-      "color: #303030",
-      "overflow: hidden",
-      "background-color: rgba(0, 0, 0, 0)",
-      "z-index: 999999",
-    ]
-  }
-
-  const parentCSS = `
-    all: initial;
-    box-sizing: border-box;
-    position: absolute;
-    display: none;
-    width: 200px;
-    height: auto;
-    font-size: 14px;
-    color: #303030;
-    overflow: hidden;
-    background-color: rgba(0, 0, 0, 0);
-    z-index: 999999;
-    border: 0 none !important;
-  `
-
-  const formCSS = `
-    all: initial;
-    border-radius: 4px 4px 4px 0px;
-    overflow: hidden;
-    display: flex;
-    flex-direction: row;
-    border: 0 none !important;
-  `
-
-  const resultsCSS = `
-    all: initial;
-    display: flex;
-    flex-direction: column;
-    background-color: #ebebeb;
-    width: 150px;
-    border-radius: 0 0 4px 4px;
-    border: 0 none !important;
-  `
-
-  const sourceLangCSS = `
-    all: initial;
-    box-sizing: border-box;
-    width: 150px;
-    height: 35px;
-    padding-left: 8px;
-    background-color: #ebebeb;
-  `
-
-  const btnCSS = `
-    all: initial;
-    box-sizing: border-box;
-    overflow: hidden;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-    text-align: center;
-    white-space: nowrap;
-    text-decoration: none !important;
-    text-transform: none;
-    text-transform: capitalize;
-    user-select: none;
-    padding: 4px 4px;
-    width: 50px;
-    height: 35px;
-    border: 0 none;
-    cursor: pointer;
-    transition: all 150ms linear;
-    color: #fff;
-    font-size: 15px;
-    justify-content: center;
-    align-items: center;
-    background: #416dea;
-    color: #FFFFFF;
-    outline: none;
-  `
-
-  function createTranslator(dictionary) {
+  function createTranslator(dictionary, cache) {
+    const id = "tb-1347589031475"
     const parent = document.createElement("div")
-    parent.style.cssText = parentCSS
+    parent.id = id
     parent.addEventListener("mouseup", e => e.stopPropagation())
 
     const form = document.createElement("div")
-    form.style.cssText = formCSS
+    form.classList.add("form")
     parent.appendChild(form)
 
+    const input = document.createElement("input")
+    input.type = "text"
+    input.placeholder = "..."
+    form.appendChild(input)
+
+    const offlineBtn = document.createElement("button")
+    // offlineBtn.innerHTML = "&#8595;"
+    offlineBtn.classList.add("offlineBtn")
+    offlineBtn.style.setProperty("background-image", `url(${browser.extension.getURL("./da2.svg")})`, "important")
+    form.appendChild(offlineBtn)
+
+    const onlineBtn = document.createElement("button")
+    // onlineBtn.innerHTML = "Nenalezeno<br />Zkusit internet?"
+    onlineBtn.classList.add("onlineBtn")
+    onlineBtn.style.setProperty("background-image", `url(${browser.extension.getURL("./ta.svg")})`, "important")
+    form.appendChild(onlineBtn)
+
     const results = document.createElement("div")
-    results.style.cssText = resultsCSS
+    results.classList.add("results")
     parent.appendChild(results)
 
-    const sourceLang = document.createElement("input")
-    sourceLang.type = "text"
-    sourceLang.placeholder = "..."
-    sourceLang.style.cssText = sourceLangCSS
-    form.appendChild(sourceLang)
+    const offlineResults = document.createElement("div")
+    offlineResults.classList.add("offlineResults")
+    results.appendChild(offlineResults)
 
-    const btn = document.createElement("button")
-    btn.textContent = "GO"
-    btn.style.cssText = btnCSS
-    form.appendChild(btn)
-
-    btn.addEventListener("click", () => {
-      const word = sourceLang.value.toLowerCase()
+    offlineBtn.addEventListener("click", () => {
+      const word = input.value.toLowerCase()
       const translations = removeDuplicates(dictionary[word] || [])
-      resetResults(translations, results)
+      setResults(translations, offlineResults)
     })
 
-    btn.addEventListener("mouseover", () => {
-      btn.style.background = "#7192ef"
+    let fetching = false
+    onlineBtn.addEventListener("click", () => {
+      const word = input.value.toLowerCase()
+
+      if (cache[word]) {
+        setResults(cache[word], offlineResults)
+      } else if(!fetching) {
+        fetching = true
+        fetch(`https://glosbe.com/gapi/translate?from=eng&dest=ces&format=json&phrase=${encodeURI(word)}`)
+          .then(res => res.json())
+          .then(json => {
+            const translations = json.tuc
+              .filter(item => item.phrase)
+              .map(item => item.phrase.text)
+
+            setResults(translations, offlineResults)
+            cache[word] = translations
+          })
+          .catch(err => console.error("English-Czech-Dictionary", err))
+          .then(() => fetching = false)
+      }
     })
 
-    btn.addEventListener("mousedown", () => {
-      btn.style.background = "#436fea"
-    })
-
-    btn.addEventListener("mouseleave", () => {
-      btn.style.background = "#416dea"
-    })
-
-    btn.addEventListener("mouseup", () => {
-      btn.style.background = "#416dea"
-    })
+    onlineBtn.addEventListener("keypress", e => e.preventDefault())
 
     return parent
   }
@@ -166,10 +94,10 @@
       .split(/\s+/)[0]
     console.log(`>${selectedText}<`)
 
-    resetResults([], translator.childNodes[1])
+    setResults([], translator.childNodes[1].firstChild)
 
     if (selectedText === "") {
-      translator.style.display = "none"
+      translator.style.setProperty("display", "none", "important")
       return
     }
 
@@ -177,19 +105,34 @@
     const y = e.pageY
 
     const top = y + 20
-    const left = Math.max(0, Math.min(x - 30, document.body.clientWidth))
+    const left = Math.max(0, x - 30)
 
-    translator.style.top = `${top}px`
-    translator.style.left = `${left}px`
-    translator.style.display = "block"
+    translator.style.setProperty("top", `${top}px`, "important")
+    translator.style.setProperty("left", `${left}px`, "important")
+    translator.style.setProperty("display", "block", "important")
     translator.firstChild.firstChild.value = selectedText
   }
 
-  window
-    .fetch(browser.extension.getURL("dictionary.json"))
-    .then(res => res.json())
-    .then(dictionary => {
-      const translator = createTranslator(dictionary)
+  function insertStyle(css) {
+    const head = document.head
+    const style = document.createElement("style")
+
+    style.type = "text/css"
+    style.appendChild(document.createTextNode(css))
+    head.appendChild(style)
+  }
+
+  const cache = {}
+  const cssPromise = fetch(browser.extension.getURL("styles.css"))
+  const dictionaryPromise = fetch(browser.extension.getURL("dictionary.json"))
+
+  Promise.all([cssPromise, dictionaryPromise])
+    .then(([css, dictionary]) => Promise.all([css.text(), dictionary.json()]))
+    .then(([css, dictionary]) => {
+
+      insertStyle(css)
+
+      const translator = createTranslator(dictionary, cache)
 
       document.body.appendChild(translator)
       document.addEventListener("mouseup", e => setTimeout(() => copySelection(e, translator), 50))
@@ -203,9 +146,5 @@
         }
       })
     })
-    .catch(err => console.error(err))
+    .catch(err => console.error("English-Czech-Dictionary", err))
 })()
-
-// const execPlugin = () => {
-// }
-// execPlugin()
